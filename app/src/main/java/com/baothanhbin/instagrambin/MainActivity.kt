14ce9134b10225772af23758cec55e3e81cde8d9
BinK <141394569+TVBinK@users.noreply.github.com>
2025-07-22 19:00:57 +0700

    leran git

--- app/src/main/java/com/baothanhbin/instagrambin/MainActivity.kt
+++ app/src/main/java/com/baothanhbin/instagrambin/MainActivity.kt
@@ -61,6 +61,7 @@ import com.baothanhbin.instagrambin.navigation.Screen
 
 
 class MainActivity : ComponentActivity() {
+    //Test git nhe
     // Đổi sang Triple để lưu callId, callerId, callerName
     private val openVideoCallState = mutableStateOf<Triple<String, String, String?>?>(null)
     // Flag để nhớ đang xử lý video call từ notification
